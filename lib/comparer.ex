53006d7e1a8f91adccd5b5bd63f1971ba6310b34
Bernardo Caputo <bernardocaputo@gmail.com>
2023-10-01 10:55:52 -0300

    initial commit
    Date:      Sun Oct 1 10:55:52 2023 -0300

---
+++ lib/comparer.ex
@@ -0,0 +1,18 @@
+defmodule Comparer do
+  @moduledoc """
+  Documentation for `Comparer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Comparer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/comparer_test.exs
@@ -0,0 +1,8 @@
+defmodule ComparerTest do
+  use ExUnit.Case
+  doctest Comparer
+
+  test "greets the world" do
+    assert Comparer.hello() == :world
+  end
+end
